bb726392291ef44e1d927fc533f1d0fb53946a24
sarliss <sinaramartins@gmail.com>
2020-05-22 12:14:16 -0400

    Updated Api-helper baseURL to heroku server

--- client/src/services/Api-helper.js
+++ client/src/services/Api-helper.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
-  //baseURL: "https://test.herokuapp.com"
+  // baseURL: "http://localhost:3000"
+  baseURL: "https://pure-island-69489.herokuapp.com"
 })
 
 // LOGIN
