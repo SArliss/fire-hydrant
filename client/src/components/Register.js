60d9136a4684770c76db32e9a94303dcae6560a7
sarliss <sinaramartins@gmail.com>
2020-06-03 16:24:26 -0400

    Refactored register page style

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -29,102 +29,104 @@ export default class Register extends Component {
 
   render() {
     return (
-      <div className="register">
+      <div className="login-page">
         {this.props.errorText && <p>{this.props.errorText}</p>}
 
         <header>
-          <div className="datadog-logo-register">
+          <div className="header-logo">
             <img src={logo} alt="datadog logo" ></img>
           </div>
-          <div className="header-description">
+          <div className="header-title">
             <h1>New Account</h1>
             <h3>Create an account so we can tailor the content for you.</h3>
           </div>
         </header>
 
-        <div className="register-form login-page">
-          <div className="register-page-left login-page-left">
-            <form onSubmit={(e) => this.props.handleRegister(e, this.state)}>
-              {this.state.errorText && <p className="error-text">{this.state.errorText}</p>}
-              <br></br>
-              <section className="uploading-images">
-                <input
-                  type="text"
-                  name="first_img"
-                  value={this.state.first_img}
-                  onChange={this.handleChange}
-                  placeholder="Primary Image URL"
-                />
-                <input
-                  type="text"
-                  name="second_img"
-                  value={this.state.second_img}
-                  onChange={this.handleChange}
-                  placeholder="Secondary Image URL"
-                />
-              </section>
-              <br></br>
-              <input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handleChange}
-                placeholder="Name"
-              />
-              <br></br>
-              <input
-                type="text"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                placeholder="Email"
-              />
-              <br></br>
-              <input
-                type="text"
-                name="location"
-                value={this.state.location}
-                onChange={this.handleChange}
-                placeholder="Location"
-              />
-              <br></br>
-              <input
-                type="text"
-                name="position"
-                value={this.state.position}
-                onChange={this.handleChange}
-                placeholder="Position"
-              />
-              <br></br>
-              <input
-                type="text"
-                name="fun_facts"
-                value={this.state.fun_facts}
-                onChange={this.handleChange}
-                placeholder="Fun Facts"
-              />
-              <br></br>
-              <input
-                type="password"
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                placeholder="Password"
-              />
-              <br></br>
-              <button>Sign up</button>
-              <section>
-                <div className="or-section"><p>or</p></div>
-                <Link to="/login"><p>Login</p></Link>
-              </section>
-            </form>
-          </div>
+        <main>
+          <form className="login-form" onSubmit={(e) => this.props.handleRegister(e, this.state)}>
+            {this.state.errorText && <p className="error-text">{this.state.errorText}</p>}
 
-          <div className="register-page-right login-page-right">
-            <img src={dogo} alt="nobody knows you are a dog" id="dog-by-computer"></img>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              placeholder="Name"
+            />
+
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="Email"
+            />
+
+            <input
+              type="text"
+              name="location"
+              value={this.state.location}
+              onChange={this.handleChange}
+              placeholder="Location"
+            />
+
+            <input
+              type="text"
+              name="position"
+              value={this.state.position}
+              onChange={this.handleChange}
+              placeholder="Position"
+            />
+
+            <input
+              type="text"
+              name="fun_facts"
+              value={this.state.fun_facts}
+              onChange={this.handleChange}
+              placeholder="Fun Facts"
+            />
+
+            <section className="uploading-images">
+              <input
+                type="text"
+                name="first_img"
+                value={this.state.first_img}
+                onChange={this.handleChange}
+                placeholder="Primary Image URL"
+              />
+              <input
+                type="text"
+                name="second_img"
+                value={this.state.second_img}
+                onChange={this.handleChange}
+                placeholder="Secondary Image URL"
+              />
+            </section>
+
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              placeholder="Password"
+            />
+
+            <button>Sign up</button>
+
+            <section>
+              <div className="or-section"><p>or</p></div>
+              <Link to="/login"><p>Login</p></Link>
+            </section>
+
+          </form>
+
+
+          <div className="login-picture">
+            <img src={dogo} alt="nobody knows you are a dog" id="dog-meeting"></img>
             <h6>Copyright 2016 The New Yorker Image by Danny Shanahan. <br></br>All Rights Reserved.</h6>
           </div>
-        </div>
+        </main>
+
       </div>
     )
   }
