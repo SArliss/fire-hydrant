7b08c234f8dad486bd7ec236d055e1f770728078
sarliss <sinaramartins@gmail.com>
2020-06-17 03:08:03 -0400

    Created readAllArticles, createArticle and deleteArticle functions. Added map in render

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -4,17 +4,43 @@ import UserProfile from './UserProfile';
 import Pack from './Pack';
 import UserLoggedOut from './UserLoggedOut';
 import Announcements from './Announcements';
+import { getArticles, postArticle, deleteArticleCall, verifyUser } from '../services/Api-helper';
 
 class UserPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      yourPosts: false,
-      allPosts: false,
-      pack: true,
-      form: false,
-      announcements: false
+      articles: [],
+      showYourPosts: true,
+      showAllPosts: false,
+      pack: false,
+      form: true,
+      announcements: true
     }
+  }
+
+  readAllArticles = async () => {
+    const articles = await getArticles();
+    this.setState({ articles });
+  }
+
+  createArticle = async (articleData) => {
+    const newArticle = await postArticle(articleData);
+    this.setState({
+      todos: [...this.state.articles, newArticle]
+    })
+    this.readAllArticles();
+  }
+
+  deleteArticle = async (e, articleId) => {
+    e.preventDefault();
+    await deleteArticleCall(articleId);
+    this.readAllArticles();
+  }
+
+  componentDidMount() {
+    verifyUser();
+    this.readAllArticles();
   }
 
   render() {
@@ -27,15 +53,33 @@ class UserPage extends React.Component {
             <div className="user-feed">
 
               <nav>
-                <button onClick={e => this.setState({ pack: false, form: true, announcements: true, yourPosts: true, allPosts: false })}>Your Posts</button>
-                <button onClick={e => this.setState({ pack: false, form: true, announcements: true, yourPosts: false, allPosts: true })}>All Posts</button>
-                <button onClick={e => this.setState({ pack: true, form: false, announcements: false, yourPosts: false, allPosts: false })}>Pack</button>
-                <button onClick={e => this.setState({ pack: false, form: false, announcements: true, yourPosts: false, allPosts: false })}>Global</button>
+                <button onClick={e => this.setState({ pack: false, form: true, announcements: true, showYourPosts: true, showAllPosts: false })}>Your Posts</button>
+                <button onClick={e => this.setState({ pack: false, form: true, announcements: true, showYourPosts: false, showAllPosts: true })}>All Posts</button>
+                <button onClick={e => this.setState({ pack: true, form: false, announcements: false, showYourPosts: false, showAllPosts: false })}>Pack</button>
+                <button onClick={e => this.setState({ pack: false, form: false, announcements: true, showYourPosts: false, showAllPosts: false })}>Global</button>
               </nav>
 
               <main>
-                {this.state.form && <FeedForm />}
+                {this.state.form && <FeedForm createArticle={this.createArticle} />}
                 {this.state.pack && <Pack />}
+
+                {this.state.showYourPosts &&
+                  <div className="articles-wrapper">
+                    <h3>Your posts</h3>
+                    <div className="articles-container">
+                      {this.state.articles && this.state.articles.map(article => (
+                        <div key={article.id} className="single-article">
+                          <p>{article.title}</p>
+                          <div className="single-article-bottom">
+                            <button className="delete"
+                              onClick={e => this.deleteArticle(e, article.id)}>Delete
+                        </button>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                }
               </main>
 
               {this.state.announcements && <Announcements />}
